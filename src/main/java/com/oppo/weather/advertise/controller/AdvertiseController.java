abc6dec4c77365d14fe77efa8689a399301b17c9
shenyao <shenyao@myoas.com>
2019-07-26 18:34:12 +0800

    还原

--- src/main/java/com/oppo/weather/advertise/controller/AdvertiseController.java
+++ src/main/java/com/oppo/weather/advertise/controller/AdvertiseController.java
@@ -8,10 +8,8 @@ import com.oppo.weather.advertise.service.AdvertiseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -24,7 +22,7 @@ import java.util.Random;
 import java.util.UUID;
 
 
-@RestController
+@Controller
 @RequestMapping(path = "/advertise")
 public class AdvertiseController {
 
@@ -36,30 +34,39 @@ public class AdvertiseController {
     @Autowired
     private ImageProperties imageProperties;
 
+    private  String message;
+
     @RequestMapping(path = "index",method = RequestMethod.GET)
     public String advertiseIndex(){ //返回广告上传页面
         return "advertise";
     }
 
     @RequestMapping(path = "upload",method=RequestMethod.POST)
+    @ResponseBody
     public ResponseResult uploadAd(Advertise advertise, @RequestParam("file") MultipartFile file){
+        ResponseResult responseResult;
+        message = null;
         String imgName = saveImg(file);
         if(Objects.isNull(imgName)){
-            return new ResponseResult("000","上传失败");
+            responseResult = new ResponseResult("002",message);
         }else{
             advertise.setAdIconName(imgName);
             advertise.setLocationKey(getRandomString(20));
             advertise.setAdPos((short)3);
             advertise.setUpdateTime(new Date());
             boolean returnVal = advertiseService.uploadAd(advertise);
-            return new ResponseResult("001","上传成功");
+            if(returnVal){
+                responseResult = new ResponseResult("000","上传成功");
+            }
+            responseResult =  new ResponseResult("001","上传失败");
         }
+        return responseResult;
     }
 
     /**
      * 保存上传图片到指定目录
      * @param file 图片文件
-     * @return 保存成功后,随机生成的文件名
+     * @return 保存成功后,返回随机生成的文件名
      */
     private String saveImg(MultipartFile file) {
         String imgName=null;
@@ -71,9 +78,11 @@ public class AdvertiseController {
                 imgName = UUID.randomUUID().toString();
                 file.transferTo(new File(imageProperties.getImgDir() + imgName + imageProperties.getSuffix()));
             }else{
+                message = "图片尺寸不合格,请上传height="+imageProperties.getHeight()+",width="+imageProperties.getWidth()+"规格的图片";
                 logger.warn("图片尺寸不合格,请上传height={},width={}规格的图片",imageProperties.getHeight(),imageProperties.getWidth());
             }
         } catch (IOException e) {
+            message = "图片上传异常";
             logger.error("图片上传异常,e={}",e.getMessage());
         }
         return imgName;
